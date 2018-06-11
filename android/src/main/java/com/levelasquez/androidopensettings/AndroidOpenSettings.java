91e6ce8e83d71be0099650f4835beb0d295610d0
honzababarik <honza@develite.io>
2018-06-10 21:14:39 -0700

    Update AndroidOpenSettings.java

--- android/src/main/java/com/levelasquez/androidopensettings/AndroidOpenSettings.java
+++ android/src/main/java/com/levelasquez/androidopensettings/AndroidOpenSettings.java
@@ -56,7 +56,7 @@ public class AndroidOpenSettings extends ReactContextBaseJavaModule {
     
     @ReactMethod
     public void notificationsSettings() {
-        Intent intent = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+        Intent intent = new Intent("android.settings.APP_NOTIFICATION_SETTINGS");
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         intent.setData(Uri.parse("package:" + reactContext.getPackageName()));
